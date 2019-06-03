13d66354b4f60edb94191e419fde56ab5ad356f1
jjolano <jjolano@me.com>
2019-06-03 00:27:04 -0400

    make Shadow load first, use Cephei for preference handling

--- ShadowPreferences/SHDWRootListController.h
+++ ShadowPreferences/SHDWRootListController.h
@@ -1,29 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
-
-@interface NSTask : NSObject
-
-@property (copy) NSArray *arguments;
-@property (copy) NSString *currentDirectoryPath;
-@property (copy) NSDictionary *environment;
-@property (copy) NSString *launchPath;
-@property (readonly) int processIdentifier;
-@property (retain) id standardError;
-@property (retain) id standardInput;
-@property (retain) id standardOutput;
-
-+ (id)currentTaskDictionary;
-+ (id)launchedTaskWithDictionary:(id)arg1;
-+ (id)launchedTaskWithLaunchPath:(id)arg1 arguments:(id)arg2;
-
-- (id)init;
-- (void)interrupt;
-- (bool)isRunning;
-- (void)launch;
-- (bool)resume;
-- (bool)suspend;
-- (void)terminate;
-
-@end
+#import <CepheiPrefs/HBRootListController.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <Cephei/HBPreferences.h>
+#import "../Includes/Shadow.h"
 
 @interface SHDWRootListController : PSListController
 - (void)generate_map:(id)sender;
