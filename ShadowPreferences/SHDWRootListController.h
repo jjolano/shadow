432b5ff0ba690b1159f1cfb8b44d6045f7b572ee
jjolano <jjolano@me.com>
2019-06-13 00:22:20 -0400

    clean up, use cepheiprefs

--- ShadowPreferences/SHDWRootListController.h
+++ ShadowPreferences/SHDWRootListController.h
@@ -2,16 +2,12 @@
 #import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
 #import <CepheiPrefs/HBRootListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
 #import <Cephei/HBPreferences.h>
 #import <Cephei/HBRespringController.h>
 #import "../Includes/Shadow.h"
 
-@interface SHDWRootListController : PSListController
+@interface SHDWRootListController : HBRootListController
 - (void)generate_map:(id)sender;
-- (void)support_reddit:(id)sender;
-- (void)support_github:(id)sender;
-- (void)support_paypal:(id)sender;
 - (void)respring:(id)sender;
 - (void)reset:(id)sender;
 @end
