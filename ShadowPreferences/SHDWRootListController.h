962ed91c39b1aa5d5279c8aa5052d32346b350f1
jjolano <jjolano@me.com>
2019-06-17 01:55:01 -0400

    use sbreload if available, make things safer

--- ShadowPreferences/SHDWRootListController.h
+++ ShadowPreferences/SHDWRootListController.h
@@ -6,6 +6,8 @@
 #import <Cephei/HBRespringController.h>
 #import "../Includes/Shadow.h"
 
+#include <spawn.h>
+
 @interface SHDWRootListController : HBRootListController
 - (void)generate_map:(id)sender;
 - (void)respring:(id)sender;
