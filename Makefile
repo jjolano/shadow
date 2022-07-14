c693c81adf44a6a84a386707569689dc8af27dad
jjolano <jjolano@me.com>
2022-07-14 00:35:23 -0400

    some stuff

--- Makefile
+++ Makefile
@@ -9,12 +9,12 @@ TWEAK_NAME = Shadow
 ShadowHooks = $(wildcard hooks/*.x)
 
 Shadow_FILES = $(ShadowHooks) Tweak.x
-Shadow_LIBRARIES = rocketbootstrap
+# Shadow_LIBRARIES = rocketbootstrap
 Shadow_EXTRA_FRAMEWORKS = Cephei
 Shadow_PRIVATE_FRAMEWORKS = AppSupport
-Shadow_CFLAGS = -fobjc-arc
+Shadow_CFLAGS = -fobjc-arc -DROCKETBOOTSTRAP_LOAD_DYNAMIC
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 SUBPROJECTS += shadowd
-SUBPROJECTS += shadowsettings
+SUBPROJECTS += shadowprefs
 include $(THEOS_MAKE_PATH)/aggregate.mk
--- shadowd/main.m
+++ shadowd/main.m
@@ -1,14 +1,7 @@
 #import "Shadow.h"
 
-#include <sys/snapshot.h>
-#include <sqlite3.h>
-
 #import <rocketbootstrap/rocketbootstrap.h>
 #import <AppSupport/CPDistributedMessagingCenter.h>
-
-static void create_origfs_map(void) {
-
-}
 
 int main(int argc, char *argv[], char *envp[]) {
 	@autoreleasepool {
@@ -19,22 +12,13 @@ int main(int argc, char *argv[], char *envp[]) {
 			return 1;
 		}
 
-		// Check new install file.
-		if([[NSFileManager defaultManager] fileExistsAtPath:@"/Library/Shadow/.new_install"]) {
-			// (re) Create the orig-fs map.
-			create_origfs_map();
-
-			// Delete new install file.
-			[[NSFileManager defaultManager] removeItemAtPath:@"/Library/Shadow/.new_install" error:nil];
-		}
-
 		// Start RocketBootstrap server.
 		CPDistributedMessagingCenter* messagingCenter = [CPDistributedMessagingCenter centerNamed:@"me.jjolano.shadow"];
 		rocketbootstrap_distributedmessagingcenter_apply(messagingCenter);
 		[messagingCenter runServerOnCurrentThread];
 
 		// Register messages.
-		[messagingCenter registerForMessageName:@"isRestrictedPath" target:shadow selector:@selector(handleMessageNamed:withUserInfo:)];
+		[messagingCenter registerForMessageName:@"shadowd_isRestricted" target:shadow selector:@selector(handleMessageNamed:withUserInfo:)];
 
 		// Keep daemon running.
 		[[NSRunLoop currentRunLoop] run];
