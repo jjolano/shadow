9e1ade438faf37c9ba84baf9b1fb20bf9b7e4a68
jjolano <jjolano@me.com>
2022-12-26 14:29:13 -0500

    small optimization

--- api/Shadow+Utilities.m
+++ api/Shadow+Utilities.m
@@ -18,15 +18,17 @@
     // check if path is symlink
     NSString* path_tmp = path;
 
-    while(![path_tmp isEqualToString:@"/"]) {
-        struct stat buf;
-        int (*original_lstat)(const char* pathname, struct stat* buf) = lstat_ptr;
+    struct stat buf;
+    int (*original_lstat)(const char* pathname, struct stat* buf) = lstat_ptr;
 
-        if(original_lstat([path UTF8String], &buf) != -1 && buf.st_mode & S_IFLNK) {
-            return YES;
+    if(original_lstat) {
+        while(![path_tmp isEqualToString:@"/"]) {
+            if(original_lstat([path UTF8String], &buf) != -1 && buf.st_mode & S_IFLNK) {
+                return YES;
+            }
+
+            path_tmp = [path_tmp stringByDeletingLastPathComponent];
         }
-
-        path_tmp = [path_tmp stringByDeletingLastPathComponent];
     }
     
     return NO;
