1e24d47cf62879c6694611ef1393568210ee843d
jjolano <jjolano@me.com>
2023-01-13 01:50:40 -0500

    reorganize files

--- install_to_theos.sh
+++ install_to_theos.sh
@@ -2,5 +2,5 @@
 set -e
 rm -rf "$THEOS/lib/HookKit.framework"
 mkdir -p "$THEOS/lib/HookKit.framework"
-cp -v "Resources/HookKit.tbd" "$THEOS/lib/HookKit.framework/HookKit.tbd"
+cp -v "HookKit.tbd" "$THEOS/lib/HookKit.framework/HookKit.tbd"
 cp -v "HookKit.h" "$THEOS/include/HookKit.h"
