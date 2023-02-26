6f68ffb987f9eaf57c60a24cfe164251a6272ed5
jjolano <jjolano@me.com>
2023-02-26 13:38:35 -0500

    add control

--- install_to_theos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-rm -rf "$THEOS/lib/HookKit.framework"
-mkdir -p "$THEOS/lib/HookKit.framework"
-cp -v "HookKit.tbd" "$THEOS/lib/HookKit.framework/HookKit.tbd"
-cp -v "Headers/HookKit.h" "$THEOS/include/HookKit.h"
