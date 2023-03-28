2e9f6e70972a625134d16b24cb58383eba504bda
jjolano <jjolano@me.com>
2023-03-27 22:57:35 -0400

    update build script for latest theos

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 PWD=$(dirname -- "$0")
 cd $PWD
@@ -7,14 +8,18 @@ cd $PWD
 rm -rf $PWD/build
 mkdir -p $PWD/build
 
-# build main project
-for d in deb/iphoneos-* ; do
-    (cd $d && make package FINALPACKAGE=1 && cp -p "`ls -dtr1 packages/* | tail -1`" ../../build/)
-done
+# build main project (rootless ver.)
+make clean &&
+THEOS_PACKAGE_SCHEME=rootless ARCHS="arm64 arm64e" TARGET=iphone:clang:latest:14.0 make package FINALPACKAGE=1 &&
+cp -p "`ls -dtr1 packages/* | tail -1`" $PWD/build/
+
+# build main project (rooted ver.)
+make clean &&
+make package FINALPACKAGE=1 &&
+cp -p "`ls -dtr1 packages/* | tail -1`" $PWD/build/
 
 # build modules
 for d in Modules/*.bundle ; do
-    (for dd in $d/deb/iphoneos-* ; do
-        (cd $dd && make package FINALPACKAGE=1 && cp -p "`ls -dtr1 packages/* | tail -1`" ../../../../build/)
-    done)
+    (cd $d && make clean && make package FINALPACKAGE=1 && cp -p "`ls -dtr1 packages/* | tail -1`" ../../build/) || true
+    (cd $d && make clean && THEOS_PACKAGE_SCHEME=rootless ARCHS="arm64 arm64e" TARGET=iphone:clang:latest:14.0 make package FINALPACKAGE=1 && cp -p "`ls -dtr1 packages/* | tail -1`" ../../build/) || true
 done
