7a0f7f10e80929e5a3f5fe965be5b0583af7d72d
NyaMisty <misty@misty.moe>
2023-11-12 14:02:38 +0800

    Support RootHide

--- build.sh
+++ build.sh
@@ -10,7 +10,12 @@ mkdir -p $PWD/build
 
 # build main project (rootless ver.)
 make clean &&
-THEOS_PACKAGE_SCHEME=rootless ARCHS="arm64 arm64e" TARGET=iphone:clang:latest:14.0 make package FINALPACKAGE=1 &&
+THEOS_PACKAGE_SCHEME=rootless ARCHS="arm64 arm64e" TARGET=iphone:clang:14.5:14.0 make package FINALPACKAGE=1 &&
+cp -p "`ls -dtr1 packages/* | tail -1`" $PWD/build/
+
+# build main project (roothide ver.)
+make clean &&
+THEOS_PACKAGE_SCHEME=roothide ARCHS="arm64 arm64e" TARGET=iphone:clang:14.5:14.0 make package FINALPACKAGE=1 &&
 cp -p "`ls -dtr1 packages/* | tail -1`" $PWD/build/
 
 rm -rf $THEOS/lib/Shadow.framework
